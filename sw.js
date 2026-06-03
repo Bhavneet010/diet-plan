a22dafb48703fcbc6d3e69deee18d6e62f5dd9eb
Bhavneet010 <Bhavneet010@users.noreply.github.com>
2026-06-03 22:54:24 +0530

    Polish soft glow design and fix mobile charts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "glowplan-v2";
+const CACHE = "glowplan-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
