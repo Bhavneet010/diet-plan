6c1b7949ec6a829ba443046ef173d08c12d27300
Bhavneet010 <Bhavneet010@users.noreply.github.com>
2026-06-03 23:17:59 +0530

    Tighten spacing and improve weight logging row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dietplan-v8";
+const CACHE = "dietplan-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
