b037eac487ec34328ef342dc273ec3a2bf5106ae
Bhavneet010 <Bhavneet010@users.noreply.github.com>
2026-06-03 22:46:51 +0530

    Apply soft glow journal theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "glowplan-v1";
+const CACHE = "glowplan-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
