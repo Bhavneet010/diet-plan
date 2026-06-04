decc9a908706b031a00ff96db29ca3f42260f72f
Bhavneet010 <Bhavneet010@users.noreply.github.com>
2026-06-04 19:16:31 +0530

    Add editable meal times

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dietplan-v12";
+const CACHE = "dietplan-v13";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
