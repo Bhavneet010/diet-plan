af574f08cd4ed741b72f4b9c2f9d45172e50093b
Bhavneet010 <Bhavneet010@users.noreply.github.com>
2026-06-04 19:19:39 +0530

    Make meal times collapsible

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dietplan-v13";
+const CACHE = "dietplan-v14";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
